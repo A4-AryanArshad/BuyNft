adf7111337f4bb19494b2f88b0b85bce05d57e43
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2024-12-03 12:38:41 +0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Components from './Components/Components';
+
+function App() {
+  return (
+<Components/>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Components.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Components.css';
+import { FaHandsHelping } from "react-icons/fa";
+import { CiClock1 } from "react-icons/ci";
+const Components = () => {
+  return (
+  <>
+  <div id="iy">
+    <div id="loy">
+        <img src="Buy.avif"/>
+    </div>
+
+    <div id="loy2">
+<h1>2023 SAGE #30 Bijan Robinson (CGC 8.5 NM-MT+)</h1>
+<p>Owned by 
+Krakratotatox</p>
+
+<div id="carder">
+    <div id='upperinfo'>
+    <CiClock1  id="lp"/>
+    <p>Sale ends December 29, 2024 at 4:04 AM </p>
+
+    </div>
+    <hr></hr>
+    <div id="innercarder">
+        <p>Current price</p>
+        <h3>$6.00</h3>
+        <button>Buy Now</button>
+    </div>
+    <div id="support">
+        <div id="sec">
+    <FaHandsHelping id="hand"/>
+        <h2>Supports creator 1$</h2>
+        </div>
+        <p>This listing is paying the collection creator their suggested creator earnings.</p>
+    </div>
+</div>
+
+    </div>
+   
+  </div>
+  <div id="description">
+    <h2>Description</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit distinctio iure quia neque culpa nisi! Praesentium numquam repellat, doloremque odio, sunt aut, et qui asperiores enim officiis modi possimus. Modi.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit distinctio iure quia neque culpa nisi! Praesentium numquam repellat, doloremque odio, sunt aut, et qui asperiores enim officiis modi possimus. Modi.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit distinctio iure quia neque culpa nisi! Praesentium numquam repellat, doloremque odio, sunt aut, et qui asperiores enim officiis modi possimus. Modi.</p>
+</div>
+
+  </>
+  )
+}
+
+export default Components
